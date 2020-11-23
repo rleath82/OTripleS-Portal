ed93bcb800b98fe07a4f6e50e60d0baffa0eb86d
Robert Leatherman <r.leatherman82@gmail.com>
2020-11-22 21:40:31 -0500

    ShouldThrowCriticalDependencyExceptionOnRegisterIfUrlNotFoundErrorOccursAndLogItAsync -> FAIL

--- OTripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exceptions.cs
+++ OTripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exceptions.cs
@@ -44,5 +44,39 @@ namespace OTripleS.Portal.Web.Tests.Unit.Services.Students
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();            
         }
+
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRegisterIfUrlNotFoundErrorOccursAndLogItAsync()
+        {
+            // given
+            Student someStudent = CreateRandomStudent();
+            string exceptionMessage = GetRandomString();
+            var responseMessage = new HttpResponseMessage();
+
+            var httpResponseUrlNotFoundException = new HttpResponseUrlNotFoundException(responseMessage: responseMessage, message: exceptionMessage);
+
+            var expectedDependencyException = new StudentDependencyException(httpResponseUrlNotFoundException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostStudentAsync(It.IsAny<Student>()))
+                    .ThrowsAsync(httpResponseUrlNotFoundException);
+            // when
+            ValueTask<Student> registerStudentTask = this.studentService.RegisterStudentAsync(someStudent);
+
+            // then
+            await Assert.ThrowsAsync<StudentDependencyException>(() =>
+                registerStudentTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostStudentAsync(It.IsAny<Student>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedDependencyException))),
+                    Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ OTripleS.Portal.Web/Models/Exceptions/StudentDependencyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OTripleS.Portal.Web.Models.Exceptions
+{
+    public class StudentDependencyException : Exception
+    {
+        public StudentDependencyException(Exception innerException)
+            : base("student dependency error occurred, contact support.", innerException) { }
+    }
+}
