29766312a5f633fe5dd66871ce86b8f14a4f03bc
Robert Leatherman <r.leatherman82@gmail.com>
2020-11-22 22:38:25 -0500

    ShouldThrowDependencyExceptionOnRegisterIfServerInternalErrorOccursAndLogItAsync -> FAIL

--- OTripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exceptions.cs
+++ OTripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exceptions.cs
@@ -104,5 +104,40 @@ namespace OTripleS.Portal.Web.Tests.Unit.Services.Students
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRegisterIfServerInternalErrorOccursAndLogItAsync()
+        {
+            //given
+            Student someStudent = CreateRandomStudent();
+            string exceptionMessage = GetRandomString();
+            var responseMessage = new HttpResponseMessage();
+
+            var httpResponseInternalServerErrorException = new HttpResponseInternalServerErrorException(responseMessage: responseMessage, message: exceptionMessage);
+
+            var expectedStudentDependencyException = new StudentDependencyException(httpResponseInternalServerErrorException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostStudentAsync(It.IsAny<Student>()))
+                    .ThrowsAsync(httpResponseInternalServerErrorException);
+
+            //when
+            ValueTask<Student> registerStudentTask = this.studentService.RegisterStudentAsync(someStudent);
+
+            //then
+            await Assert.ThrowsAsync<StudentDependencyException>(() =>
+                registerStudentTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostStudentAsync(It.IsAny<Student>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedStudentDependencyException))),
+                    Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
